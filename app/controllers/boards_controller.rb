38a2f49edb92aa33332bd7e4c4733582d6f58098
node-gif <at-resort.0404@outlook.jp>
2020-04-08 14:21:16 +0000

    投稿から一覧ページに戻るまでの設定

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,14 +1,27 @@
 class BoardsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @board = Board.all
   end
 
   def show
-    @board = Board.find_by(params[:id])
+    @board = Board.find_by(id: params[:id])
   end
 
   def new
     @board = Board.new
+  end
+  
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      flash[:notice] = "投稿に成功しました"
+      redirect_to @board
+    else
+      flash[:alert] = "投稿に失敗しました"
+      render new_board_path
+    end
   end
 
   def edit
@@ -17,6 +30,6 @@ class BoardsController < ApplicationController
   private
   
     def board_params
-      params.require(:board).permit(:title, :body)
+      params.require(:board).permit(:title, :body).merge(user_id: current_user.id)
     end
 end
