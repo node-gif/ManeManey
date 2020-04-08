03ac333732ba772eefe2f8b3d8fe02a7f0a852c7
node-gif <at-resort.0404@outlook.jp>
2020-04-08 13:00:52 +0000

    新規登録ページと投稿詳細ページの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,11 +4,19 @@ class BoardsController < ApplicationController
   end
 
   def show
+    @board = Board.find_by(params[:id])
   end
 
   def new
+    @board = Board.new
   end
 
   def edit
   end
+  
+  private
+  
+    def board_params
+      params.require(:board).permit(:title, :body)
+    end
 end
