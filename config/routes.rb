954fe374c212220c55f34f702240803dd78bb060
node-gif <at-resort.0404@outlook.jp>
2020-04-08 06:40:30 +0000

    boardsコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'boards/index'
+  get 'boards/show'
+  get 'boards/new'
+  get 'boards/edit'
   devise_for :users,
     controllers: { registrations: 'registrations' }
     
