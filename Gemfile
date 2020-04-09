a803717414572fdb483d6c1af76c0e329b661259
node-gif <at-resort.0404@outlook.jp>
2020-04-09 09:26:12 +0000

    コメント機能の実装

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'kaminari'
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry-rails'
+  gem 'rails-flog', require: 'flog'
 end
 
 group :development do
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      flash[:notice] = "コメントしました"
+      redirect_to @board
+    else
+      render :@board
+    end
   end
 
   def destroy
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:comment, :user_id, :board_id)
   end
 end
